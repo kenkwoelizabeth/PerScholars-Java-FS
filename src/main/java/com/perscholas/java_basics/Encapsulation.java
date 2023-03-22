8d907d46d470b6df6c9ec5a2bec22a28a4923e8d
Elizabeth <kenkwoelizabeth78@gmail.com>
2023-03-22 15:02:30 -0400

    This is my sixth commit

--- src/main/java/com/perscholas/java_basics/Encapsulation.java
+++ src/main/java/com/perscholas/java_basics/Encapsulation.java
@@ -3,7 +3,8 @@ package com.perscholas.java_basics;
 public class Encapsulation {
     private int age;
     private String name;
-
+    private int height;
+    private int width;
 
     public void setAge(int a) {
         age = a;
@@ -23,15 +24,33 @@ public class Encapsulation {
     public String getName() {
         return name;
     }
+    public void setHeight(int h) {
+        height = h;
+    }
+
+
+    public int getHeight() {
+        return height;
+    }
+    public void setWidth(int w) {
+        width = w;
+    }
+
+
+    public int getWidth() {
+        return width;
+    }
 
 
         public static void main (String[]args){
             Encapsulation sa = new Encapsulation();
               sa.setAge(20);
               sa.setName("Lizzy");
-
+              sa.setHeight(50);
+              sa.setWidth(10);
+              int rectangleArea= sa.getHeight() * sa.getWidth();
             System.out.println(sa.getName() + " is " + sa.getAge() + " years old " );
-            System.out.println(sa.getName() + " is " + sa.getAge() + " years old " );
+            System.out.println( "The area of the rectangle is " + rectangleArea);
 
         }
 }
